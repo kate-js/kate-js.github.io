2f42b7ec69e68f0ccd4e66a2c99bd45f17a0a0cb
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2024-02-05 10:16:12 +0100

    feat: create pages

---
+++ index.js
@@ -0,0 +1,21 @@
+//modal window
+let modal = document.getElementById("myModal");
+
+let btn = document.getElementById("flowerCard");
+
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+//burger-menu
